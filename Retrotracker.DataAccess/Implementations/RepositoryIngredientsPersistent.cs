5f7e8fc5ee82b8257c5834696abaf4782ea8b182
Alberto Salas <alsalasher@gmail.com>
2023-12-07 20:56:08 +0100

    change ingredients repo

--- Retrotracker.DataAccess/Implementations/RepositoryIngredientsPersistent.cs
+++ Retrotracker.DataAccess/Implementations/RepositoryIngredientsPersistent.cs
@@ -1,86 +1,12 @@
 ﻿using Retrotracker.Domain;
-using System.Text.Json;
 
 namespace Retrotracker.DataAccess;
-
-public class RepositoryIngredientsPersistent : IRepositoryIngredients
+public class RepositoryIngredientsPersistent : RepositoryPersistent<Ingredient>, IRepositoryIngredients
 {
-    private readonly string _storageFileName = "ingredientsStorage.json";
-    private readonly string _path;
-    public RepositoryIngredientsPersistent()
-    {
-        _path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LocalStorage", _storageFileName);
-    }
-
-    public Ingredient Add(Ingredient entity)
-    {
-        var allIngredients = GetAll().ToList();
-
-        if (allIngredients.FindIndex(x => x.Name == entity.Name) != -1)
-        {
-            return entity;
-        }
-
-        allIngredients.Add(entity);
-        SaveData(allIngredients);
-        return entity;
-    }
-
-    public bool Delete(Ingredient entity)
-    {
-        var allIngredients = GetAll().ToList();
-        var result = allIngredients.Remove(entity);
-        if (result)
-        {
-            SaveData(allIngredients);
-        }
-        return result;
-    }
-
-    public IEnumerable<Ingredient> GetAll()
-    {
-        return GetDeserializedItems();
-    }
-
-    public Ingredient? GetByID(string id)
-    {
-        return GetAll().FirstOrDefault(x => x.Id == id);
-    }
+    public RepositoryIngredientsPersistent(string jsonFileName) : base(jsonFileName) { }
 
     public IEnumerable<Ingredient> GetByIDs(List<string> ids)
     {
         return GetAll().Where(x => ids.Contains(x.Id));
     }
-
-    public Ingredient Update(Ingredient entity)
-    {
-        var allIngredients = GetAll().ToList();
-        var ingredientIndex = allIngredients.FindIndex(x => x.Id == entity.Id);
-
-        if (ingredientIndex != -1)
-        {
-            allIngredients[ingredientIndex] = entity;
-        }
-        else
-        {
-            allIngredients.ToList().Add(entity);
-        }
-
-        SaveData(allIngredients);
-        return entity;
-    }
-
-    private void SaveData(IEnumerable<Ingredient> ingredients)
-    {
-        var payloadAsString = JsonSerializer.Serialize(ingredients);
-        File.WriteAllText(_path, payloadAsString);
-    }
-
-    private IEnumerable<Ingredient> GetDeserializedItems()
-    {
-        string payload = File.ReadAllText(_path);
-        List<Ingredient>? deserializeItems = JsonSerializer.Deserialize<List<Ingredient>>(payload);
-        return deserializeItems ?? new List<Ingredient>();
-    }
-
 }
