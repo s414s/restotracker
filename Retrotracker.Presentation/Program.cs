a85245b0f58f05fe0604b4c705b33d51bb1f8b44
Alberto Salas <alsalasher@gmail.com>
2023-12-08 17:57:03 +0100

    add manual DI

--- Retrotracker.Presentation/Program.cs
+++ Retrotracker.Presentation/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Retrotracker.Application;
 using Retrotracker.DataAccess;
 using Retrotracker.Domain;
+using Retrotracker.Presentation;
 
 Console.WriteLine("Hello, World!");
 
@@ -17,3 +18,23 @@ serviceCollection.AddSingleton<IRepository<Ingredient>, RepositoryIngredientsPer
 serviceCollection.AddSingleton<IRepository<Dish>, RepositoryDishesPersistent>();
 
 Console.WriteLine("Dependency injection done!");
+
+// TODO - preguntar si esto iria en capa CrossCutting
+RepositoryUsersPersistent repoUsers = new();
+RepositoryOrdersPersistent repoOrders = new();
+RepositoryIngredientsPersistent repoIngredients = new();
+RepositoryDishesPersistent repoDishes = new(repoIngredients);
+
+UserServices userServices = new(repoUsers);
+OrderServices orderServices = new(repoOrders);
+DishServices dishServices = new();
+
+ConsoleLogger console = new(
+    userServices,
+    orderServices,
+    dishServices
+);
+
+console.Run();
+
+Console.WriteLine("Finnishing!");
